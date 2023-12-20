9d21536a8847237ce18ea3069ebcec05cb1d42a8
Julian Klett <julian22a@googlemail.com>
2023-12-20 11:12:24 +0100

    cleanup

--- pages/create.js
+++ pages/create.js
@@ -4,7 +4,7 @@ import useSWR from "swr";
 import { Form } from "@/components/form/Form.js";
 import styled from "styled-components";
 
-const StyledContent = styled.div`
+const StyledContent = styled.section`
   padding-top: 3.5rem;
   padding-bottom: 3.5rem;
 `;
@@ -14,7 +14,7 @@ const CreateContainer = styled.div`
   background-color: #1e2124;
   color: #61dafb;
   position: relative;
-  margin-left: 1rem;
+  margin-left: 0.8rem;
   margin-right: 1rem;
 `;
 
